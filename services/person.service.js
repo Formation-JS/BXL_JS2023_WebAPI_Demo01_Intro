9ee780ab46043fa9d2f3836e0024785ce9aa8534
Aurélien <aurelien.strimelle@bstorm.be>
2024-03-07 15:25:13 +0100

    Update FakeData "Person" 🙀

--- services/person.service.js
+++ services/person.service.js
@@ -8,9 +8,25 @@ const fakeData = {
             lastname: 'Duck',
             confirm: true,
             nbGuest: 4
+        },
+        {
+            personId: 2,
+            email: 'balthazar.picsou@digitalcity.brussels',
+            firstname: 'Balthazar',
+            lastname: 'Picsou',
+            confirm: false,
+            nbGuest: null
+        },
+        {
+            personId: 4,
+            email: 'miss.tick@digitalcity.brussels',
+            firstname: 'Miss Tick',
+            lastname: 'De Sortilege',
+            confirm: true,
+            nbGuest: 1
         }
     ],
-    nextId: 2
+    nextId: 5
 }
 
 const personService = {
